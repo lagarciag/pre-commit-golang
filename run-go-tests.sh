089314ed3db125f2a840baf6ec6406ff328faced
Luis Garcia <lagarciag@gmail.com>
2016-05-04 10:12:35 -0600

    chg: dev: Adding hook for running tests

---
+++ run-go-tests.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+set -e -u -o pipefail # Fail on error
+#gometalinter --deadline=60s --vendor --cyclo-over=20 --dupl-threshold=100 ./...
+GORACE="halt_on_error=1" LD_LIBRARY_PATH=/usr/local/lib go test -v -race github.hpe.com/GoP4Sim/clintsim/tests
